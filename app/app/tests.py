7a24e39994502da5bb4f69b6ff25711955ccc8ec
root <lseirina87@gmail.com>
2023-04-15 12:39:55 +0300

    added tests for user API

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,17 +3,20 @@ Samplr tests
 """
 from django.test import SimpleTestCase
 
+
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
     
     def test_add_number(self):
-        res = calc.add(5,6)
-        
+        res = calc.add(5, 6)
+    
         self.assertEqual(res, 11)
-        
+    
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
-        
+    
         self.assertEqual(res, 5)
+        
+    
